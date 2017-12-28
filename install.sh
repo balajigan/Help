9a832b1897d9cc68bf3252aa809d2742321a5c94
balajigan <balaji_inf@yahoo.com>
2017-12-28 17:07:19 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+
+sudo apt-get install -y openjdk-8-jdk
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+
+export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
+
+sudo apt-get install -y git-core
+
