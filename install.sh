8fef27ed4aab43cc35f68eeb748f7a4440e84b71
balajigan <balaji_inf@yahoo.com>
2017-12-28 21:41:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
 sudo apt-get install -y git-core
 sudo apt-get install -y maven
+mkdir /opt/jmeter
+cd /opt/jmeter
+curl -O http://apache.spinellicreations.com//jmeter/binaries/apache-jmeter-3.3.tgz
+tar -xvzf apache-jmeter-3.3.tgz
+
+
 git clone https://github.com/balajigan/cache-rest-service.git
 cd cache-rest-service/hazelcast-cache-service
 
