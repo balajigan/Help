de6b73234fd0bcf0b5c5e34a893c4762905a981c
balajigan <balaji_inf@yahoo.com>
2017-12-28 21:25:38 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export PATH=$PATH:/usr/lib/jvm/java-8-openjdk-amd64/bin
 sudo apt-get install -y git-core
 sudo apt-get install -y maven
+https://github.com/balajigan/cache-rest-service.git
+cd cache-rest-service/hazelcast-cache-service
+
 git clone https://github.com/balajigan/Hazelcast.git
 
