5c9a55168955a5e69a044acb6f382f7b1200dff2
balajigan <balaji_inf@yahoo.com>
2018-02-01 13:57:03 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,6 +60,8 @@ case $1 in
 	cd /opt/camunda
 	curl -O -k https://camunda.org/release/camunda-bpm/wildfly10/7.8/camunda-bpm-wildfly10-7.8.0.tar.gz
 	tar -xvzf camunda-bpm-wildfly10-7.8.0.tar.gz
+	sed -i -e "s/jboss.bind.address.management:127.0.0.1/jboss.bind.address.management:$IP_ADDRESS/g" /opt/camunda/server/wildfly-10.1.0.Final/standalone/configuration/standalone.xml
+	sed -i -e "s/jboss.bind.address.management:127.0.0.1/jboss.bind.address.management:$IP_ADDRESS/g" /opt/camunda/server/wildfly-10.1.0.Final/standalone/configuration/standalone.xml
 	;;
    *)
         echo "Unknown"
