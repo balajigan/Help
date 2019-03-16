2e88af5a4354cbe4853d4ff9738ed5cf63b8ea4c
balajigan <balaji_inf@yahoo.com>
2019-03-16 11:12:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -262,6 +262,17 @@ case $1 in
 	sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 	echo "Access the server at : http://servers_public_IP"
         ;;
+	docker)
+        echo "Installing docker"
+	sudo apt-get update
+	sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+	sudo apt-key fingerprint 0EBFCD88
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+	sudo apt-get update
+	sudo apt-get install docker-ce docker-ce-cli containerd.io
+	sudo docker run hello-world
+	;;
    *)
         echo "Unknown"
         ;;
