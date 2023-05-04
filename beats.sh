16f8788dc743891e99d9e8f1e159c8ab2ca3be02
balajigan <balaji_inf@yahoo.com>
2023-05-04 15:56:45 -0500

    Update beats.sh

--- beats.sh
+++ beats.sh
@@ -3,6 +3,26 @@
 case $1 in
    kafka)
       echo "configure Kafka"
+      echo "Take backup of filebeat.yml"
+      cp /opt/filebeat/filebeat-8.0.1/filebeat.yml /opt/filebeat/filebeat-8.0.1/filebeat.bak.elastic.yml
+      echo "Stop filebeat"
+      sudo systemctl stop filebeat_eck
+      echo "TODO - copy the file"
+      echo "Start filebeat"
+      sudo systemctl start filebeat_eck
+      echo "Status of filebeat service"
+      sudo systemctl status filebeat_eck.service
+
+      echo "Take backup of metricbeat.yml"
+      cp /opt/metricbeat/metricbeat-8.0.1/metricbeat.yml /opt/metricbeat/metricbeat-8.0.1/metricbeat.bak.elastic.yml
+      echo "Stop metricbeat"
+      sudo systemctl stop metricbeat_eck
+      echo "TODO - copy the file"
+      echo "Start metricbeat"
+      sudo systemctl start metricbeat_eck
+      echo "Status of metricbeat service"      
+      sudo systemctl status  metricbeat_eck.service
+
       ;;
    
    *)
